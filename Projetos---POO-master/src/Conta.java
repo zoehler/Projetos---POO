f51e600c1f53a093e0c0032ad30ade65552565f8
nicolasbenke <39104837+nicolasbenke@users.noreply.github.com>
2018-05-26 04:20:46 -0300

    Adição das classes ContaCorrente e ContaEspecial
    Criei as classes ContaCorrente e ContaEspecial, e dei update na classe Conta que tava dando erro devido ao saldo no construtor da mesma.

---
+++ Projetos---POO-master/src/Conta.java
@@ -0,0 +1,80 @@
+public abstract class Conta {
+
+    String numeroConta;
+    String nomeCliente;
+    String cpfCliente;
+    double saldo;
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    private void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public String getCpfCliente() {
+        return cpfCliente;
+    }
+
+    private void setCpfCliente(String cpfCliente) {
+        this.cpfCliente = cpfCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    private void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Conta(String numeroConta, String nomeCliente, String cpfCliente) {
+        setNumeroConta(numeroConta);
+        setNomeCliente(nomeCliente);
+        setCpfCliente(cpfCliente);
+    }
+
+
+    public boolean sacar(double valorSacado) {
+
+        if(valorSacado <= getSaldo() && valorSacado > 0){
+            setSaldo(getSaldo()-valorSacado);
+            return true;
+        } else {
+            return false;
+        }
+
+
+    }
+
+
+    public boolean depositar(double valorDepositado) {
+
+        if(valorDepositado > 0){
+            setSaldo(getSaldo()+valorDepositado);
+            return true;
+        } else {
+            return false;
+        }
+
+
+    }
+
+    public String imprimir() {
+
+        return "\nNum. Conta: " + getNumeroConta()
+                + "\nNome: " + getNomeCliente()
+                + "\nCPF: " + getCpfCliente()
+                + "\nSaldo: " + getSaldo();
+
+    }
+}
