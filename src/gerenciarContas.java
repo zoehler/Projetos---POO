4d850999a0ce96b8bf0b648ac21e905a0d3d4a3d
Lucas Luigi <35871207+LucasLuigiT@users.noreply.github.com>
2018-05-27 16:38:31 -0300

    Adição dos métodos adicionar e remover contas
    Adição dos métodos na classe gerenciarContas

--- src/gerenciarContas.java
+++ src/gerenciarContas.java
@@ -4,5 +4,19 @@ public class gerenciarContas {
 
     ArrayList<Conta> contas = new ArrayList<>();
 
+    public void adicionarConta(Conta c){
+        contas.add(c);
+    }
+
+    public boolean removerConta(int numeroConta){
+        for (int i=0;i<=contas.size();i++){
+            if (contas.get(i).getNumeroConta()==numeroConta){
+                contas.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 }
